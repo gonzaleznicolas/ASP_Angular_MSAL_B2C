c25aa92bc1fd8f5c2099808cb94843d73b4173a3
Nicolas Gonzalez <nicolas.gonzalez@arcurve.ca>
2021-10-01 15:03:00 -0600

    backend gets hit but error thrown

--- ASP_Angular_B2C_Test/Controllers/WeatherForecastController.cs
+++ ASP_Angular_B2C_Test/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace ASP_Angular_B2C_Test.Controllers
 {
-    //[Authorize]
+    // [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
