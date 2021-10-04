af96ffd6c2cf09429d4ec5dfa3de3a8828bc6c42
Nicolas Gonzalez <nicolas.gonzalez@arcurve.ca>
2021-10-04 12:46:23 -0600

    unsubscribe from some msal events

--- ASP_Angular_B2C_Test/ClientApp/src/app/home/home.component.ts
+++ ASP_Angular_B2C_Test/ClientApp/src/app/home/home.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
 import { EventMessage, EventType, InteractionStatus } from '@azure/msal-browser';
-import { filter } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   loginDisplay = false;
+  private readonly _destroying$ = new Subject<void>();
 
   constructor(private authService: MsalService, private msalBroadcastService: MsalBroadcastService) { }
 
@@ -16,6 +18,7 @@ export class HomeComponent implements OnInit {
     this.msalBroadcastService.msalSubject$
       .pipe(
         filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS),
+        takeUntil(this._destroying$)
       )
       .subscribe((result: EventMessage) => {
         console.log(result);
@@ -23,7 +26,8 @@ export class HomeComponent implements OnInit {
 
     this.msalBroadcastService.inProgress$
       .pipe(
-        filter((status: InteractionStatus) => status === InteractionStatus.None)
+        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        takeUntil(this._destroying$)
       )
       .subscribe(() => {
         this.setLoginDisplay();
@@ -32,5 +36,10 @@ export class HomeComponent implements OnInit {
 
   setLoginDisplay() {
     this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
   }
 }
